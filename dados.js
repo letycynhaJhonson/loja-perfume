1bbf143011891c81f8c6d0ac92746102e0b8ddc5
eduarda <160619678+letycynhaJhonson@users.noreply.github.com>
2025-03-25 14:43:19 -0300

    at

---
+++ dados.js
@@ -0,0 +1,52 @@
+let dados = [
+    {
+        nome: "Chanel N°5",
+        descricao: "Um clássico atemporal que combina elegância e sofisticação em cada gota.",
+        link: "#"
+    },
+    {
+        nome: "Dior Sauvage",
+        descricao: "Fresco e poderoso, inspirado na natureza selvagem.",
+        link: "#"
+    },
+    {
+        nome: "Versace Eros",
+        descricao: "Uma fragrância intensa e sedutora para os apaixonados.",
+        link: "#"
+    },
+    {
+        nome: "Tom Ford Black Orchid",
+        descricao: "Luxuoso e misterioso, uma fusão de escuridão e sensualidade.",
+        link: "#"
+    },
+    {
+        nome: "Armani Code",
+        descricao: "Elegância e charme masculino em uma fragrância sedutora.",
+        link: "#"
+    },
+    {
+        nome: "Yves Saint Laurent Libre",
+        descricao: "A liberdade em forma de perfume, ousado e vibrante.",
+        link: "#"
+    },
+    {
+        nome: "Gucci Bloom",
+        descricao: "Uma viagem ao coração de um jardim encantado.",
+        link: "#"
+    },
+    {
+        nome: "Prada Candy",
+        descricao: "Doce e viciante, perfeito para quem ama ousadia.",
+        link: "#"
+    },
+    {
+        nome: "Calvin Klein Eternity",
+        descricao: "Amor eterno em uma fragrância clássica e sofisticada.",
+        link: "#"
+    },
+    {
+        nome: "Burberry Her",
+        descricao: "Moderno, jovem e vibrante, inspirado em Londres.",
+        link: "#"
+    }
+];
---
+++ script.js
@@ -0,0 +1,32 @@
+function pesquisar() {
+    let section = document.getElementById('resultados-pesquisa');
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+    campoPesquisa = campoPesquisa.toLowerCase();
+
+    if (campoPesquisa == "") {
+        section.innerHTML = "<p>Nenhum texto digitado!</p>";
+        return;
+    }
+
+    let resultados = "";
+    for (let dado of dados) {
+        let nome = dado.nome.toLowerCase();
+        let descricao = dado.descricao.toLowerCase();
+
+        if (nome.includes(campoPesquisa) || descricao.includes(campoPesquisa)) {
+            resultados += `
+                <div class="item-resultado">
+                    <h2><a href="${dado.link}" target="_blank">${dado.nome}</a></h2>
+                    <p class="descricao-meta">${dado.descricao}</p>
+                    <a href="${dado.link}" target="_blank" rel="noopener noreferrer">Mais Informações</a>
+                </div>
+            `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Resultado não Encontrado</p>";
+    }
+
+    section.innerHTML = resultados;
+}
